6f58f896006122fbd9c44e6a27df8e5875a15a55
Jeshanth-Vaz <60683382+Jeshanth-Vaz@users.noreply.github.com>
2020-02-11 11:57:24 +0530

    Add files via upload

---
+++ Prog1.java
@@ -0,0 +1,19 @@
+package exam; 
+ public class Prog1 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+
+            for (int j = 5 - i; j > 1; j--) {
+                // printing spaces
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j <= i; j++) {
+                // printing stars
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+    }
+}
